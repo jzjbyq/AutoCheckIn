f052d958f68ac67dc4d3771890b03102bc176561
jzjbyq <jzjbyq@qq.com>
2023-06-13 14:50:12 +0800

    登录网址更新

--- laowang.py
+++ laowang.py
@@ -11,7 +11,7 @@ cookie抓包 wordpress_sec_xxxxxxxxxx 即可
 #老王资源部落签到
 0 8 * * * https://raw.githubusercontent.com/jzjbyq/AutoCheckIn/main/laowangziyuan.py, tag=老王资源部落签到, enabled=true
 [rewrite_local]
-https://www.laowang555.com url script-request-header https://raw.githubusercontent.com/jzjbyq/AutoCheckIn/main/laowangziyuan.py
+https://www.laowang2021.com url script-request-header https://raw.githubusercontent.com/jzjbyq/AutoCheckIn/main/laowangziyuan.py
 """
 
 import re
@@ -38,15 +38,15 @@ def start(ck):
     for i in payload:
         name = re.findall('.*\=(.*?)[\%\|]', i)[0]
         headers = {
-            'authority': 'www.laowang555.com',
+            'authority': 'www.laowang2021.com',
             'accept': 'application/json, text/javascript, */*; q=0.01',
             'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
             'cache-control': 'no-cache',
             'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
             'cookie': i,
-            'origin': 'https://www.laowang555.com',
+            'origin': 'https://www.laowang2021.com',
             'pragma': 'no-cache',
-            'referer': 'https://www.laowang555.com/',
+            'referer': 'https://www.laowang2021.com/',
             'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Microsoft Edge";v="108"',
             'sec-ch-ua-mobile': '?0',
             'sec-ch-ua-platform': '"Windows"',
@@ -60,11 +60,12 @@ def start(ck):
         data = {
             'action': 'user_checkin',
         }
-        # {'msg': '连续4天签到成功！ 积分+20 经验值+5', 'data': {'integral': 5, 'points': 20, 'time': '2022-11-18 15:51:55'}, 'continuous_day': 4, 'details_link': '<a data-class="modal-mini" mobile-bottom="true" data-height="240" data-remote="https://www.laowang555.com/wp-admin/admin-ajax.php?action=checkin_details_modal" class=" checkin-details-link" href="javascript:;" data-toggle="RefreshModal"></a>', 'error': False}
-        res = requests.post('https://www.laowang555.com/wp-admin/admin-ajax.php', headers=headers,
+        # {'msg': '连续4天签到成功！ 积分+20 经验值+5', 'data': {'integral': 5, 'points': 20, 'time': '2022-11-18 15:51:55'}, 'continuous_day': 4, 'details_link': '<a data-class="modal-mini" mobile-bottom="true" data-height="240" data-remote="https://www.laowang2021.com/wp-admin/admin-ajax.php?action=checkin_details_modal" class=" checkin-details-link" href="javascript:;" data-toggle="RefreshModal"></a>', 'error': False}
+        res = requests.post('https://www.laowang2021.com/wp-admin/admin-ajax.php', headers=headers,
                             data=data).json()
         if res == 0:
             print(name, '的Cookie已过期')
+            send_content += f'{name} 的Cookie已过期\n'
             continue
         msg = res['msg']
 
@@ -73,7 +74,7 @@ def start(ck):
             'action': 'checkin_details_modal',
         }
         try:
-            res = requests.get('https://www.laowang555.com/wp-admin/admin-ajax.php', params=params, headers=headers)
+            res = requests.get('https://www.laowang2021.com/wp-admin/admin-ajax.php', params=params, headers=headers)
             qday = re.findall(r'<badge class="c-blue">(.*)</badge>', res.text)[0]
             # print(qday)
         except:
@@ -81,7 +82,7 @@ def start(ck):
 
         # 获取当前积分数
         try:
-            res = requests.get('https://www.laowang555.com/user/balance', headers=headers)
+            res = requests.get('https://www.laowang2021.com/user/balance', headers=headers)
             rhtml = etree.HTML(res.text)
             integral = rhtml.xpath('//*[@id="user-tab-balance"]/div[1]/div[1]/div/div/div[2]/div[1]/span/text()')[0]
         except Exception as e:
